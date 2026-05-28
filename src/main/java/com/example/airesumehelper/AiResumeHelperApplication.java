9d87cb0768002c32873bdddc3c2ef2a5c75b1aef
zky <1083747354@qq.com>
2026-05-28 12:33:49 +0800

    完成简历ai助手项目

--- src/main/java/com/example/airesumehelper/AiResumeHelperApplication.java
+++ src/main/java/com/example/airesumehelper/AiResumeHelperApplication.java
@@ -1,6 +1,5 @@
 package com.example.airesumehelper;
 
-import org.springframework.ai.autoconfigure.openai.OpenAiAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AiResumeHelperApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(AiResumeHelperApplication.class, args);
     }
 
